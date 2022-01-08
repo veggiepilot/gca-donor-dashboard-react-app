a5ca3ba3f5a842c3b8c71ebef30f4d4e8aed9fb6
Rebecca Sundquist <rsundquist629@gmail.com>
2022-01-07 21:16:55 -0500

    added student data card

--- client/src/components/studentDataCard.js
+++ client/src/components/studentDataCard.js
@@ -1,17 +1,41 @@
-import Container from "react-bootstrap/Container"
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
+import React from "react";
+import { Card, Table } from "react-bootstrap";
 
-const studentDataCard = () => {
-    <Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src="holder.js/100px180" />
-  <Card.Body>
-    <Card.Title>Card Title</Card.Title>
-    <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-    </Card.Text>
-    <Button variant="primary">Go somewhere</Button>
-  </Card.Body>
-</Card>
+export default function App() {
+  return (
+    <Card>
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Table>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Username</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>@fat</td>
+            </tr>
+            <tr>
+              <td>3</td>
+              <td colSpan="2">Larry the Bird</td>
+              <td>@twitter</td>
+            </tr>
+          </tbody>
+        </Table>
+      </Card.Body>
+    </Card>
+  );
 }
