3eff5428779964e64f335da3fb7cf6fd11619031
Adam <adamolsey>
2022-01-08 12:07:15 -0500

    removed imports

--- client/src/components/dropDownComponents/sideBar.js
+++ client/src/components/dropDownComponents/sideBar.js
@@ -1,6 +1,3 @@
-import Container from "react-bootstrap/Container";
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
 import Accordion from "react-bootstrap/Accordion";
 import Nav from "react-bootstrap/Nav";
 
