cf590759411563854bdb50c80e7dc2ce0e6051b0
Rebecca Sundquist <rsundquist629@gmail.com>
2022-01-10 20:55:43 -0500

    added styling to donors and pledge

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
 import SignIn from "./views/signIn";
 import Header from "./components/header";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { fa-solid fa-user-graduate } from '@fortawesome/free-solid-svg-icons'
+
+
 import { 
   ApolloClient, 
   InMemoryCache, 
   ApolloProvider 
 } from "@apollo/client";
+
+library.add(fab, faUserGraduate)
+
 
 //Apollo client setup
 const client = new ApolloClient({
--- client/src/components/cards/cardComponent.js
+++ client/src/components/cards/cardComponent.js
@@ -1,5 +1,9 @@
 import React from "react";
 import { Card, Container, } from "react-bootstrap";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+
+
 
 const CardComponent = () => {
   return (
@@ -7,9 +11,9 @@ const CardComponent = () => {
 <Container className=" d-flex flex-row justify-content-center align-content-center">
   
 <Card className="text-center w-100 d-flex flex-column justify-content-center align-content-center m-2">
-  <Card.Header>Student Total</Card.Header>
+  <Card.Header>Students</Card.Header>
   <Card.Body>
-    <Card.Title>insert icon for student</Card.Title>
+    <Card.Title><i class="fas fa-user-graduate"></i></Card.Title>
     <Card.Text className="d-flex flex-row justify-content-between p-4">
     <h1 className="d-flex flex-start justify-content-start align-content-center">23</h1>
     <p className= "d-flex flex-end justify-content-end align-content-center">Total Students</p>
@@ -21,11 +25,12 @@ const CardComponent = () => {
 <Card className="text-center w-100 d-flex flex-column justify-content-center align-content-center m-2">
   <Card.Header>Donor Total</Card.Header>
   <Card.Body>
-    <Card.Title>insert icon for donor</Card.Title>
-    <Card.Text>
-      <h1>13</h1><p>Total Donors</p>
+  <Card.Title><i class="fas fa-user-graduate"></i></Card.Title>
+    <Card.Text className="d-flex flex-row justify-content-between p-4">
+    <h1 className="d-flex flex-start justify-content-start align-content-center">13</h1>
+    <p className= "d-flex flex-end justify-content-end align-content-center">Total Donors</p>
     </Card.Text>
-  </Card.Body>
+  </Card.Body>.
 </Card>
 
 
@@ -33,9 +38,10 @@ const CardComponent = () => {
 <Card className="text-center w-100 d-flex flex-column justify-content-center align-content-center m-2">
   <Card.Header>Pledge Fund Summary</Card.Header>
   <Card.Body>
-    <Card.Title>insert pledge icon</Card.Title>
-    <Card.Text>
-      <h1>$34,506</h1><p>Total Pledge</p>
+  <Card.Title><i class="fas fa-user-graduate"></i></Card.Title>
+    <Card.Text className="d-flex flex-row justify-content-between p-4">
+    <h1 className="d-flex flex-start justify-content-start align-content-center">$34,506</h1>
+    <p className= "d-flex flex-end justify-content-end align-content-center">Total Pledge</p>
     </Card.Text>
   </Card.Body>
 </Card>
