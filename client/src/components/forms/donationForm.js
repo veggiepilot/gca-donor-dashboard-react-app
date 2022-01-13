e876cc26815565545de305ab447703b567c15842
Adam <adamolsey>
2022-01-13 04:49:11 -0500

    all forms have been updated to match seeds

--- client/src/components/forms/donationForm.js
+++ client/src/components/forms/donationForm.js
@@ -12,40 +12,50 @@ const donationForm = (props) => {
         <Form>
           <Row className="mb-3">
             <Form.Group as={Col} controlId="formGridFirstName">
-              <Form.Label>Student First Name</Form.Label>
+              <Form.Label>First Name</Form.Label>
               <Form.Control type="firstname" placeholder="John" />
             </Form.Group>
 
             <Form.Group as={Col} controlId="formGridLastName">
-              <Form.Label>Student Last Name</Form.Label>
-              <Form.Control type="lastname" placeholder="Wick" />
+              <Form.Label>Last Name</Form.Label>
+              <Form.Control type="lastname" placeholder="Doe" />
             </Form.Group>
           </Row>
-
-          <Form.Group className="mb-3" controlId="formGridAddress1">
-            <Form.Label>Student Address</Form.Label>
-            <Form.Control placeholder="1234 Main St" />
-          </Form.Group>
-
-          <Form.Group className="mb-3" controlId="formGridAddress2">
-            <Form.Label>Address 2</Form.Label>
-            <Form.Control placeholder="Apartment, studio, or floor" />
-          </Form.Group>
-
           <Row className="mb-3">
+            <Form.Group as={Col} controlId="formGridAddress1">
+              <Form.Label>Address 1</Form.Label>
+              <Form.Control placeholder="1234 Main St" />
+            </Form.Group>
+
             <Form.Group as={Col} controlId="formGridCity">
               <Form.Label>City</Form.Label>
-              <Form.Control placeholder="Padhorje" />
+              <Form.Control placeholder="Dreamland" />
             </Form.Group>
-
+          </Row>
+          <Row className="mb-3">
+            <Form.Group as={Col} className="mb-3" controlId="formGridAddress2">
+              <Form.Label>Address 2</Form.Label>
+              <Form.Control placeholder="Apartment, studio, or floor" />
+            </Form.Group>
             <Form.Group as={Col} controlId="formGridState">
               <Form.Label>State</Form.Label>
-              <Form.Control placeholder="Belarus" />
+              <Form.Control placeholder="MI" />
             </Form.Group>
-
             <Form.Group as={Col} controlId="formGridZip">
               <Form.Label>Zip</Form.Label>
               <Form.Control placeholder="55505" />
+            </Form.Group>
+          </Row>
+          <Row className="mb-3"></Row>
+          <Row className="mb-3">
+            <Form.Group as={Col} controlId="formGridEmail">
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="email" placeholder="email@gmail.com" />
+            </Form.Group>
+
+            <Form.Group as={Col} controlId="formGridPhone">
+              <Form.Label>Phone Number</Form.Label>
+              <Form.Control type="phone" placeholder="(123) 123-3567" />
             </Form.Group>
 
             <Form.Group as={Col} controlId="formGridStudent">
