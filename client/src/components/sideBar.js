2626a6b8138aec954265083cb5e2fd8288314514
Rebecca Sundquist <rsundquist629@gmail.com>
2022-01-12 20:30:47 -0500

    updated name in studentcreationviews

--- client/src/components/sideBar.js
+++ client/src/components/sideBar.js
@@ -1,34 +1,40 @@
-import { Outlet, Link } from "react-router-dom";
-import { Accordion, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { Accordion, Button, Nav, Row, Col, Navbar } from "react-bootstrap";
 
 const SideBar = () => {
   return (
-    <>
-      <Link to="/dashboard">
-        <Button className="w-100 mb-4 mt-4 btn-sm">Home</Button>
-      </Link>
-      <Accordion>
-        <Accordion.Item eventKey="0" className="w-100 mb-4 mt-4">
-          <Accordion.Header>Student</Accordion.Header>
-          <Accordion.Body>
-            <Link to="/student" >Student Overveiw</Link>
-            <br></br>
-            <Link to="/createstudent">New Student</Link>
-          </Accordion.Body>
-        </Accordion.Item>
-      </Accordion>
-
-      <Accordion>
-        <Accordion.Item eventKey="1" className="w-100 mb-4 mt-4">
-          <Accordion.Header>Donor</Accordion.Header>
-          <Accordion.Body>
-            <Link to="/donor">Donor Overveiw</Link>
-            <br></br>
-            <Link to="/newdonation">New Donation</Link>
-          </Accordion.Body>
-        </Accordion.Item>
-      </Accordion>
-
+    <Navbar>
+      <Nav>
+        <Row>
+          <Col>
+            <Accordion>
+              <Accordion.Item eventKey="0" className="w-100 mb-4 mt-4">
+                <Accordion.Header>Student</Accordion.Header>
+                <Accordion.Body>
+                  <Link to="/student">Student Overveiw</Link>
+                  <br></br>
+                  <Link to="/createstudent">New Student</Link>
+                </Accordion.Body>
+              </Accordion.Item>
+            </Accordion>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Accordion>
+              <Accordion.Item eventKey="1" className="w-100 mb-4 mt-4">
+                <Accordion.Header>Donor</Accordion.Header>
+                <Accordion.Body>
+                  <Link to="/donor">Donor Overveiw</Link>
+                  <br></br>
+                  <Link to="/newdonation">New Donation</Link>
+                </Accordion.Body>
+              </Accordion.Item>
+            </Accordion>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
             <Accordion>
               <Accordion.Item eventKey="2">
                 <Accordion.Header>Account</Accordion.Header>
@@ -42,10 +48,10 @@ const SideBar = () => {
                 </Accordion.Body>
               </Accordion.Item>
             </Accordion>
-          </Nav>
-        </Navbar>
-      </Col>
-    </Row>
+          </Col>
+        </Row>
+      </Nav>
+    </Navbar>
   );
 };
 
