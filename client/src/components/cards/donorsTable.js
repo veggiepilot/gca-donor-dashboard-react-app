e877df7f2fa14ad6be432262d9b86adf41d42e1e
Rebecca Sundquist <rsundquist629@gmail.com>
2022-01-15 13:13:08 -0500

    merge with main

--- client/src/components/cards/donorsTable.js
+++ client/src/components/cards/donorsTable.js
@@ -1,31 +1,31 @@
-// import { Card } from "react-bootstrap"
-// import React from "react"
-// import { MDBContainer } from "mdbreact"
-// import { Bar } from "react-chartjs-2"
+import { Card } from "react-bootstrap"
+import React from "react"
+import { MDBContainer } from "mdbreact"
+import { Bar } from "react-chartjs-2"
   
-// const DonorsTable = () => {
+const DonorsTable = () => {
   
-//   // Sample data
-//   const data = {
-//     labels: ["January", "February", "March",
-//       "April", "May", "June", "July", "August", "September", "October", "November", "December" ],
-//     datasets: [
-//       {
-//         label: "Dollar Amount",
-//         data: [2, 5, 6, 7, 3, 3, 4],
-//         backgroundColor: "#02b844",
-//         borderWidth: 1,
-//         borderColor: "#000000",
-//       }
-//     ]
-//   }
+  // Sample data
+  const data = {
+    labels: ["January", "February", "March",
+      "April", "May", "June", "July", "August", "September", "October", "November", "December" ],
+    datasets: [
+      {
+        label: "Dollar Amount",
+        data: [2, 5, 6, 7, 3, 3, 4],
+        backgroundColor: "#02b844",
+        borderWidth: 1,
+        borderColor: "#000000",
+      }
+    ]
+  }
   
-//   return (
-//     <MDBContainer>
-//       <Bar data={data}
-//         style={{ maxHeight: '600px' }}
-//       />
-//     </MDBContainer>
-//   );
-// }
-//     export default DonorsTable; 
+  return (
+    <MDBContainer>
+      <Bar data={data}
+        style={{ maxHeight: '600px' }}
+      />
+    </MDBContainer>
+  );
+}
+    export default DonorsTable; 
